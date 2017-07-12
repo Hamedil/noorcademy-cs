ca6200769d8be76355dc9692d480346993b9a10d
Hamed Adil <hamedadil@hotmail.com>
2017-07-12 23:59:14 +0300

    Created Relationships
    Created relationships between models

--- app/CourseType.php
+++ app/CourseType.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class CourseType extends Model
 {
     //
+
+    public function trainer() {
+        return $this->belongsTo('App\Trainer');
+    }
+
 }
--- app/Trainer.php
+++ app/Trainer.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Trainer extends Model
 {
     //
+
+    public function courseTypes() {
+        return $this->hasOne('App\CourseType');
+    }
+
+
 }
