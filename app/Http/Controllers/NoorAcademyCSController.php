9db9287b3090f77500d1d5914a43fed21d3f099b
Raarujeed <mustafaraar@gmail.com>
2017-07-13 09:10:02 +0300

    Created Classes index, postTrainer, postTrainee

--- app/Http/Controllers/NoorAcademyCSController.php
+++ app/Http/Controllers/NoorAcademyCSController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\CourseType;
+use App\Trainee;
+use App\Trainer
+;
 class NoorAcademyCSController extends Controller
 {
-    //
+	/**
+	 * Display the Trainer view
+	 * Populate the view with data from CourseType Database
+	 * Return view with array
+	 * @return [array]
+	 */
+    public function index(){
+
+    }
+
+    /**
+     * Receive Trainee data from view
+     * Store the data to Trainee Database
+     * Return success/fail message
+     * @param  Request
+     * @return [string]
+     */
+    public function postTrainee(Request $request){
+    	// Validation
+    	// Store Data
+    	// Return Success/Fail Message
+    }
+
+    /**
+     * Receive Trainer data from view
+     * Store the data to Trainer Database
+     * Return success/fail message
+     * @param  Request
+     * @return [string]
+     */
+    public function postTrainer(Request $request){
+    	// Validation
+    	// Store Data
+    	// Return Success/Fail Message
+    }
 }
